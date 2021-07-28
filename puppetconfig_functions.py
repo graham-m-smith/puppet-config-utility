26a790c4b4b8b4024b3dd23f32a3381935690444
Graham Smith <gsmith@gmsnet.co.uk>
2021-07-28 19:28:12 +0100

    populate do_list

--- puppetconfig_functions.py
+++ puppetconfig_functions.py
@@ -1,2 +1,9 @@
-def do_list():
+def do_list(table_service):
     print("this is do_list")
+
+    # Get data from Azure Table
+    data = table_service.query_entities(table_name, "PartitionKey eq 'PuppetCfg'")
+
+    for record in data:
+        machine = record['RowKey']
+        print(machine)
