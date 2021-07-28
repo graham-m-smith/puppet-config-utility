15d1a95ef14bde930805fcd13680a0188877f1cb
Graham Smith <gsmith@gmsnet.co.uk>
2021-07-28 19:18:12 +0100

    Initial Commit

---
+++ puppetconfig.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import os
+import sys
+from azure.cosmosdb.table.tableservice import TableService
+
+# Check that required environment variables are set
+
+try:  
+   os.environ["PUPPETCONFIG_SA_ACCOUNT_NAME"]
+except KeyError: 
+   print("Please set the environment variable PUPPETCONFIG_SA_ACCOUNT_NAME")
+   sys.exit(1)
+
+try:  
+   os.environ["PUPPETCONFIG_TABLE_NAME"]
+except KeyError: 
+   print("Please set the environment variable PUPPETCONFIG_TABLE_NAME")
+   sys.exit(1)
+   
+try:  
+   os.environ["PUPPETCONFIG_SAS_TOKEN"]
+except KeyError: 
+   print("Please set the environment variable PUPPETCONFIG_SAS_TOKEN")
+   sys.exit(1)
+
+# Initialise Variables
+sa_account_name = os.environ.get('PUPPETCONFIG_SA_ACCOUNT_NAME')
+table_name = os.environ.get('PUPPETCONFIG_TABLE_NAME')
+sas_token = os.environ.get('PUPPETCONFIG_SAS_TOKEN')
+table_service = TableService(account_name=sa_account_name, sas_token=sas_token)
+
+def main():
+
+    # Perform function here
+    print("this is main")
+
+    # Done
+    sys.exit(0)
+
+if __name__ == '__main__':
+	main()
