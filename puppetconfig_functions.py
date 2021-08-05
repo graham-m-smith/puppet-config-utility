6daadeacd8002181ed73799660fc5395c4f79fb6
Graham Smith <gsmith@gmsnet.co.uk>
2021-08-05 04:38:19 +0100

    remove style

--- puppetconfig_functions.py
+++ puppetconfig_functions.py
@@ -86,7 +86,7 @@ def do_show_machine(table_client, machine):
     table = PrettyTable()
     table.field_names = ['Fact', 'Value']
     table.align = 'l'
-    table.set_style(prettytable.DEFAULT)
+    #table.set_style(prettytable.DEFAULT)
 
     for key in record.keys():
         # Ignore non-fact related data
